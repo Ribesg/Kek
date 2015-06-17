84118bb96d6ddca5416dc2b37b38d03f88d90296
Ribesg <ribesg@yahoo.fr>
2015-06-17 14:32:50 +0200

    Log errors and quit without waiting for lost threads

--- src/main/kotlin/fr/ribesg/kek/Main.kt
+++ src/main/kotlin/fr/ribesg/kek/Main.kt
@@ -2,5 +2,12 @@ package fr.ribesg.kek
 
 fun main(args: Array<String>) {
     System.setProperty("org.lwjgl.librarypath", "lwjgl3/lwjgl/native")
-    Game().run()
+    try {
+        Game().run()
+    } catch (satan: Throwable) {
+        satan.printStackTrace()
+        System.exit(42)
+    } finally {
+        System.exit(0)
+    }
 }
